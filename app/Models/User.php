08dc102718c3d88261f090dfc59cd6cbc00358ac
Malekmontanaimam <malekimam123@gmail.com>
2023-09-01 06:51:31 +0200

    update redme file1s

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'avatar',
         'password',
     ];
 
@@ -43,10 +44,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
-
-     protected function password(): Attribute{
-        return Attribute::make(
-            set:fn ($value)=>bcrypt($value)
-        );
-    }
+//
+    //  protected function password(): Attribute{
+        // return Attribute::make(
+            // set:fn ($value)=>bcrypt($value)
+        // );
+    // }
 }
